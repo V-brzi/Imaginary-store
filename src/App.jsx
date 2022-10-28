db98664635287611c467808986a08e6e2f5b7c28
V-brzi <vatro.brzi@gmail.com>
2022-10-28 11:56:31 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Store from "./pages/Store";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import {Routes, Route} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 
 
 function App() {
@@ -13,12 +13,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
+      <Switch>
         <Route exact path="/Imaginary-store" element={<Home />} />
-        <Route path="/Imaginary-store/store" element={<Store />} />
-        <Route path="/Imaginary-store/favorites" element={<Favorites />} />
-        <Route path="/Imaginary-store/cart" element={<Cart />} />
-      </Routes>
+        <Route path="/store" element={<Store />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/cart" element={<Cart />} />
+      </Switch>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,19 @@ function Home(){
     return(
         <main className="home">
             <h2 className="home-title">Welcome to imaginary store</h2>
-            <Link to="/Imaginary-store/store" className="link">
+            <Link to="/store" className="link">
                 <img 
                 src="https://cdn-images.zety.com/pages/retail_cover_letter_example_zety_uk_2.jpg"
                 alt="home-img" 
                 className="home-img"/>
             </Link>
-            <Link to="/Imaginary-store/favorites" className="link">
+            <Link to="/favorites" className="link">
                 <img 
                 src="https://th.bing.com/th/id/R.510a3e2699445e484feb451e62f00cf4?rik=DAKr91iQTYVaIg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-e5J59_wRA-s%2fTVlDhChyHPI%2fAAAAAAAABGw%2fg6KZJzvhU7w%2fw1200-h630-p-k-no-nu%2fcropped-line-of-hearts.jpg&ehk=c3HBpZ5BvqlJijdu6PpAaZFid8iVPsFCmN2mPOe0FVU%3d&risl=&pid=ImgRaw&r=0"
                 alt="home-img" 
                 className="home-img"/>
             </ Link>
-            <Link to="/Imaginary-store/cart" className="link">
+            <Link to="/cart" className="link">
                 <img 
                 src="https://codvo.in/images/projects/ecommerce.jpg"
                 alt="home-img" 

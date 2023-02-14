f0748b015a91851da4ebfec5418182faa83b569e
Samuel Igwe <samueligwe63@gmail.com>
2023-02-14 17:33:52 +0100

    removed comment

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -14,7 +14,7 @@ const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    // signup function 
+  
 
     const signup = () => {
          
